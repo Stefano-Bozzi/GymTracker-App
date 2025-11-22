43ea2f831bb12b42321603af25c767904bcac408
Stefano Bozzi <stefano.bozzi@gmail.com>
2025-11-22 12:19:35 +0100

    feat: add Calendar-Workout bottom navigation-bar

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(colorScheme: lightScheme),
       darkTheme: ThemeData(colorScheme: darkScheme),
       themeMode: ThemeMode.system,                    // Change theme along with system
-      home: const MyHomePage(                         // Scaffold=MyHomePage
+      home: const MyHomePage(                         // MyHomePage is widget with a state, it changes! containing the app scheleton: Scaffold
         title: 'GainFlow',
         ),
     );
@@ -58,15 +58,9 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -76,7 +70,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -98,13 +91,16 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: [
+          NavigationDestination(icon: Icon(Icons.calendar_today), label: 'Calendar'),
+          NavigationDestination(icon: Icon(Icons.fitness_center_rounded), label: 'Workouts'),
+        ],
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
