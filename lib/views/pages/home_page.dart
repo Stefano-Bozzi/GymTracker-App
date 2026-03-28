b4f45373e75b92aea8222de07de9c5dc3fbfc63e
Stefano Bozzi <stefano.bozzi@gmail.com>
2026-03-28 16:20:21 +0100

    chore: update home_page

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -9,6 +9,26 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('HOME PAGE'),);
+    return Center(
+      child: Text(
+        '''
+Private. Local. Offline.
+No cloud. No tracking. Just training.
+
+A minimalist fitness app that respects your privacy, runs locally on your device, and doesn’t collect any data.
+''',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 20, // Font size
+          fontWeight: FontWeight.w500, // Font weight
+          fontStyle: FontStyle.normal, // Font style
+          letterSpacing: 0.5, // Letter spacing
+          wordSpacing: 2.0, // Word spacing
+          height: 1.5, // Line height
+          decoration: TextDecoration.none, // Text decoration
+          decorationStyle: TextDecorationStyle.solid, // Decoration style          
+        ),
+      ),
+    );
   }
 }
